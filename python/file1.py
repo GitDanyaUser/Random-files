9aa8a24d526074b6d66a4011be3f46f19a77a31b
GitDanyaUser <danya10121985@gmail.com>
2025-03-02 21:07:26 +0200

    Update file1.py

--- python/file1.py
+++ python/file1.py
@@ -1,5 +1,14 @@
-x = 12
-if x % 2 == 0:
-  print("x is even")
-else:
-  print("x is odd")
+def chechifeven(number):
+  """ Function for check if even
+  Im just testing """
+  if not isinstance(number, int):
+    print("Number must be integer!")
+  if number % 2 == 0:
+    print(f"{number} is even")
+    return True
+  else:
+    print(f"{number} is odd")
+    return False
+
+x = 11
+print(checkifeven(x))
